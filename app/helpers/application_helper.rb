e2c0bb8ff7e4688ce5f2d830ad100dc01ae1eef0
Steven Lindberg <stevenjohnlindberg@gmail.com>
2014-01-19 18:34:46 -0800

    Replaced navigation partial with 'navigation_links' helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def navigation_links(label = 'nav', active_class = 'active')
+    pages = @cms_site.pages.published
+
+    if label
+      pages = pages.includes(:categories).where({ cms_categories: { label: label } })
+    end
+
+    content_tag :ul do
+      pages.each do |page|
+        is_active = request.original_fullpath == page.full_path
+        content = content_tag :li, class: is_active ? active_class : nil do
+          link_to_unless is_active, page.label, page.full_path
+        end
+
+        # content_tag doesn't handle arrays well, so create processed strings
+        concat content
+      end
+    end
+  end
 end
